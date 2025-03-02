ede057fe98b36507ea0bc6895532941fc14165e1
Naveen <naveen@squareshift.co>
2025-03-02 22:46:50 +0530

    Add chat in home

--- health-report-frontend/src/Components/PrivateRoute/PrivateRoute.jsx
+++ health-report-frontend/src/Components/PrivateRoute/PrivateRoute.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import { Chat } from "../Chat/Chat";
 
 export const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem("token");
 
-  return token ? children : <Navigate to="/login" />;
+  return token ? (
+    <div>
+      {children}
+      <Chat />
+    </div>
+  ) : (
+    <Navigate to="/login" />
+  );
 };
