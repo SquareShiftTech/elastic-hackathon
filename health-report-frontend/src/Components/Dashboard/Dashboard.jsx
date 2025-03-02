046dfce454b213168cbeb23e492c47299584e48e
Naveen <naveen@squareshift.co>
2025-03-02 20:47:42 +0530

    Dashboard

--- health-report-frontend/src/Components/Dashboard/Dashboard.jsx
+++ health-report-frontend/src/Components/Dashboard/Dashboard.jsx
@@ -1,3 +1,15 @@
+import React from "react";
+import "./Dashboard.css";
+
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="dashboard-canvas">
+      <iframe
+        title="canvas"
+        width={"100%"}
+        height={"100%"}
+        src="https://ss-pet-store.kb.us-central1.gcp.cloud.es.io/app/canvas/workpad/workpad-3fbb4efd-366e-4080-a354-0c85c68a90f0/page/1?__fullScreen=true&auth_provider_hint=anonymous1"
+      />
+    </div>
+  );
 };
