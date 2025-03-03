3bb8c9d7283635072fcaf8a6d0ed9999df78028d
Naveen <naveen@squareshift.co>
2025-03-03 22:45:17 +0530

    Add chat auto scroll

--- health-report-frontend/src/Components/Chat/Chat.js
+++ health-report-frontend/src/Components/Chat/Chat.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./Chat.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { fetchData } from "../../apis/fetchData";
@@ -15,23 +15,26 @@ export const Chat = () => {
   const [sessionId] = useState(Math.random().toString(36).substring(2, 15));
   const [loading, setLoading] = useState(false);
 
+  const autoScroll = useRef(null);
+
   useEffect(() => {
     localStorage.setItem("chatMessages", JSON.stringify(messages));
   }, [messages]);
 
   const handleSendMessage = async () => {
     if (loading || !input.trim()) return;
-
+    const tempInuput = input;
     const userMessage = { sender: "user", content: input };
     setMessages((prevMessages) => [...prevMessages, userMessage]);
 
     setLoading(true);
+    setInput("");
 
     try {
       const data = await fetchData({
         url: `${process.env.REACT_APP_BACKEND_DOMAIN}/chat`,
         body: {
-          query: input,
+          query: tempInuput,
           session_id: sessionId,
           user_id: userName
         }
@@ -58,6 +61,16 @@ export const Chat = () => {
       handleSendMessage();
     }
   };
+
+  useEffect(() => {
+    if (autoScroll.current) {
+      // Scroll to the bottom when a new message is added
+      autoScroll.current.scrollTo({
+        top: autoScroll.current.scrollHeight,
+        behavior: "smooth" // Enable smooth scrolling
+      });
+    }
+  }, [messages]);
 
   return (
     <div className="floating-chat">
@@ -86,7 +99,7 @@ export const Chat = () => {
               </button>
             </div>
 
-            <div className="chat-content-container">
+            <div className="chat-content-container" ref={autoScroll}>
               {messages.map((msg, index) => (
                 <div
                   key={index}
